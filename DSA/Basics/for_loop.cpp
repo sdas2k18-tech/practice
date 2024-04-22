5df536d61ffc13c8d93727f028a6add8b5670d9d
Subhasish Das <sdas2k20@gmail.com>
2024-04-22 10:42:04 +0530

    dining philosopher problem and solution

--- DSA/Basics/for_loop.cpp
+++ DSA/Basics/for_loop.cpp
@@ -1,17 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// pass by value and &address
+// pass by value 
 void doSomething(string &s){
     s[0]='t';
     cout<< s << endl;
 }
 
 int main(){
-    int arr[5];
-    for(int i=0;i<=4;i++){
-        cin>> arr[i];
-    }
-    for
+    string s= "Daz";
+    doSomething(s);
+    cout<< s << endl;
     return 0;
 }
